75b413ca8c7ef4eaf95fbfcfaf1c6b17fdca98c8
Josh Williams <linkfan03@gmail.com>
2014-11-03 13:19:41 -0600

    Initial Commit
    Basics for Web Socket in C#

---
+++ MSWSChat/MSWSChat/MSWSChatHandler.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace MSWSChat
+{
+    /// <summary>
+    /// Summary description for MSWSChatHandler
+    /// </summary>
+    public class MSWSChatHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context, string clientOnly) {
+            if (context.IsWebSocketRequest || context.IsWebSocketRequestUpgrading) {
+                context.AcceptWebSocketRequest(new MyWSHandler());
+            }
+        }
+
+        public void ProcessRequest(HttpContext context) {
+            if (context.IsWebSocketRequest || context.IsWebSocketRequestUpgrading) {
+                context.AcceptWebSocketRequest(new MyWSHandler());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MSWSChat/MSWSChat/MyWSHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace MSWSChat
+{
+    public class MyWSHandler : WebSocketHandler
+    {
+        private static WebSocketCollection clients = new WebSocketCollection();
+        public override void OnOpen()
+        {
+            
+            base.Send("Welcome from " + this.WebSocketContext.UserHostAddress);
+            clients.Add(this);
+        }
+
+        public override void OnMessage(string message)
+        {
+            //If we send some predefined string such as "DISCONNECT" and remove that client from the list of client
+            //we can keep the bicyclist, student and roadworker as strictly a sender
+            string msgBack = string.Format(
+                "You have sent {0} at {1} with {2}", message, DateTime.Now.ToLongTimeString(), Guid.NewGuid().ToString());
+            //base.Send(msgBack);
+            clients.Broadcast(message);
+        }
+
+        public override void OnClose()
+        {
+            clients.Remove(this);
+            base.OnClose();
+        }
+
+        public override void OnError()
+        {
+            clients.Remove(this);
+            base.OnError();
+        }
+    }
+}
